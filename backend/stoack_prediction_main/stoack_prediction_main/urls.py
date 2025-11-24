919fc397da550b1d54e7927689bb37d2d7b93276
Ali nasir <alinasirkbwm@gmail.com>
2025-11-24 18:52:07 +0500

    stock prediction model and resources

--- backend/stoack_prediction_main/stoack_prediction_main/urls.py
+++ backend/stoack_prediction_main/stoack_prediction_main/urls.py
@@ -20,7 +20,7 @@ from rest_framework_simplejwt.views import TokenObtainPairView , TokenRefreshVie
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-     path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('', include('account.urls'))
 
